72b8ca8edd9f5e412084ad2767aa5573d4eded22
SowmyaV <sowmyarnv@gmail.com>
2024-04-11 13:29:14 +0530

    Adding docker file

--- Docker/app.py
+++ Docker/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
 	return "Hello World!"
 
+@app.route('/name')
+def name():
+	return "Welcome Sowmya!"
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080)
